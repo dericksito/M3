76ceabd70b02046e9e911514d21123de613239c4
dericksito <derickcedeno2@icloud.com>
2024-05-15 03:14:39 -0500

    reto 18/arrow funtions

--- REACTNATIVE/ARROWFUNTIONS/funciones.js
+++ REACTNATIVE/ARROWFUNTIONS/funciones.js
@@ -1,17 +1,21 @@
-ejecutarSumar=function(){
-    let valor1=recuperarEntero("txtValor1");
+ejecutarSumar=()=>{
+   /* let valor1=recuperarEntero("txtValor1");
     let valor2=recuperarEntero("txtValor2");
     console.log(valor1+"+"+valor2);
    let resultadoSuma=sumar(valor1,valor2);
-   console.log(resultadoSuma);
+   console.log(resultadoSuma);*/
+   ejecutarOperacion(sumar);
 }
 
-ejecutarRestar=function(){
+
+
+ejecutarOperacion=(operar)=>{
     let valor1=recuperarFloat("txtValor1");
     let valor2=recuperarFloat("txtValor2");
-    console.log(valor1+"-"+valor2);
-    let resultadoResta=restar(valor1,valor2);
-    console.log(resultadoResta);
+    
+    let resultado;
+    resultado=operar(valor1,valor2);
+    console.log(resultado);
   
 }
 
@@ -25,4 +29,28 @@ restar=(n1,n2)=>{
     let resultado=n1-n2;
     return resultado;
 }
+
+imprimir=()=>{
+
+    console.log("estoy imprimiendo....");
+}
+
+saludar=()=>{
+    alert("APRENDIENDO PROGRAMACION FUNCIONAL");
+}
+
+ejecutar=(fn)=>{
+    console.log("estoy ejecutando funciones...")
+    fn();
+}
+
+testEjecutar=()=>{
+ejecutar(imprimir);
+ejecutar(saludar);
+ejecutar(()=>{
+    alert("APRENDIENDO PROGRAMACION FUNCIONAL");
+});
+  
+}
+
 
